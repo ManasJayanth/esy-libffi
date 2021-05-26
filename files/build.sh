79c2054688c8bac8a02961b8e958898fb7f63915
Manas <3097018+ManasJayanth@users.noreply.github.com>
2021-05-27 00:31:32 +0530

    Rename build.sh to files/build.sh

--- files/build.sh
+++ files/build.sh
@@ -6,12 +6,15 @@ case "$(uname -s)" in
     CYGWIN*|MINGW32*|MSYS*)
         FLAGS="$FLAGS --host x86_64-w64-mingw32"
         ;;
-    DARWIN* | Darwin*)
+    DARWIN*|Darwin*)
         FLAGS="$FLAGS --disable-dependency-tracking"
-        if [[ $(uname -m) == "arm64"* ]]; then
-            FLAGS="$FLAGS -build=aarch64-apple-darwin20.3.0"
-        fi
-        ;;
+        case "$(uname -m)" in
+          arm64*)
+          FLAGS="$FLAGS -build=aarch64-apple-darwin20.3.0"
+          ;;
+        case *)
+          ;;
+        esac
     *)
         ;;
 esac
