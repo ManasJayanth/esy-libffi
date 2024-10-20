1223a7bf60f319d1b59668d26d3c627ee83967ca
Manas Jayanth <3097018+ManasJayanth@users.noreply.github.com>
2024-10-20 19:14:21 +0530

    Use homebrew's C test program

--- esy-test/closure.c
+++ esy-test/closure.c
@@ -1,24 +1,31 @@
+// From https://github.com/Homebrew/homebrew-core/blob/5eb3a4076a7106d068b88bd8ea6b7493590c724f/Formula/lib/libffi.rb#L41-L85
+// Same license applies to this file
 #include <stdio.h>
 #include <ffi.h>
+
 /* Acts like puts with the file given at time of enclosure. */
-void puts_binding(ffi_cif *cif, unsigned int *ret, void* args[],
-                  FILE *stream)
+void puts_binding(ffi_cif *cif, void *ret, void** args, void *stream)
 {
-  *ret = fputs(*(char **)args[0], stream);
+  *(unsigned int *)ret = fputs(*(char **)args[0], (FILE *)stream);
 }
+
 int main()
 {
   ffi_cif cif;
   ffi_type *args[1];
   ffi_closure *closure;
+
   int (*bound_puts)(char *);
   int rc;
+
   /* Allocate closure and bound_puts */
   closure = ffi_closure_alloc(sizeof(ffi_closure), &bound_puts);
+
   if (closure)
     {
       /* Initialize the argument info vectors */
       args[0] = &ffi_type_pointer;
+
       /* Initialize the cif */
       if (ffi_prep_cif(&cif, FFI_DEFAULT_ABI, 1,
                        &ffi_type_uint, args) == FFI_OK)
@@ -32,7 +39,9 @@ int main()
             }
         }
     }
+
   /* Deallocate both closure, and bound_puts */
   ffi_closure_free(closure);
+
   return 0;
 }
